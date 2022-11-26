e9af341fba2391c7c5227b22336aeaad7110e4fd
Afif Akromi <massachusetts48@gmail.com>
2022-11-26 21:33:08 +0700

    add repository

---
+++ lib/app/data/providers/task/provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:todoapp/app/core/utils/keys.dart';
+import 'package:todoapp/app/data/models/task.dart';
+import 'package:todoapp/app/data/services/storage/services.dart';
+
+class TaskProvider {
+  final StorageService _storage = Get.find<StorageService>();
+
+  List<Task> readTasks() {
+    var tasks = <Task>[];
+    json
+        .decode(_storage.read(taskKey).toString())
+        .forEach((e) => tasks.add(Task.fromJson(e)));
+
+    return tasks;
+  }
+
+  void writeTasks(List<Task> tasks) {
+    _storage.write(taskKey, json.encode(tasks));
+  }
+}
---
+++ lib/app/data/services/storage/repository.dart
@@ -0,0 +1,11 @@
+import 'package:todoapp/app/data/models/task.dart';
+import 'package:todoapp/app/data/providers/task/provider.dart';
+
+class TaskRepository {
+  final TaskProvider taskProvider;
+
+  TaskRepository({required this.taskProvider});
+
+  List<Task> readTasks() => taskProvider.readTasks();
+  void writeTasks(List<Task> tasks) => taskProvider.writeTasks(tasks);
+}
