22e545f56b223d7a4461698e073a42427d82ff8d
Afif Akromi <massachusetts48@gmail.com>
2022-11-26 21:17:11 +0700

    add services

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:todoapp/app/data/services/storage/services.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    const MyApp(),
-  );
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync((() => StorageService().init()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
