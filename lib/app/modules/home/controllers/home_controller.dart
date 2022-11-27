26994a64d874358845e0fe7c1522432fa169a842
Afif Akromi <massachusetts48@gmail.com>
2022-11-28 00:29:50 +0700

    add card widgets

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,17 @@
 import 'package:get/get.dart';
+import 'package:todoapp/app/data/models/task.dart';
+import 'package:todoapp/app/data/services/storage/repository.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final TaskRepository taskRepository;
+  HomeController({required this.taskRepository});
 
-  final count = 0.obs;
+  final tasks = <Task>[].obs;
+
   @override
   void onInit() {
     super.onInit();
+    tasks.assignAll(taskRepository.readTasks());
+    ever(tasks, (_) => taskRepository.writeTasks(tasks));
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:todoapp/app/modules/home/widgets/add_card.dart';
 import '../controllers/home_controller.dart';
+import '../../../../app/core/utils/extensions.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(4.0.wp),
+              child: Text(
+                'My List',
+                style: GoogleFonts.poppins(
+                  fontSize: 24.0.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              children: [
+                AddCard(),
+              ],
+            )
+          ],
         ),
       ),
     );
